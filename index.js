abd81a290fba17e75ef51f39edc41e17a7cab53b
shukebeta <weizhong2004@gmail.com>
2021-05-12 01:06:37 +1200

    allow setting port through environment variables

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post('/api/person', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
